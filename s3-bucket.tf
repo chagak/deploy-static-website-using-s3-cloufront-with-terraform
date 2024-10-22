e267e7185ae3fbec279adbc8b91582a02c759aaa
chagak <kchaggak@gmail.com>
2024-10-21 19:29:58 -0500

    upload file directly form terraform

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -10,9 +10,10 @@ resource "aws_s3_bucket" "example" {
 
 resource "null_resource" "upload_docker_folder" {
   provisioner "local-exec" {
-    command = "aws s3 sync C:/Users/kouev/honey-static-webapp s3://${aws_s3_bucket.example.bucket} --acl private"
+    command = "aws s3 sync ./honey-static-webapp s3://${aws_s3_bucket.example.bucket} --acl private"
   }
 
   depends_on = [aws_s3_bucket.example]
 }
 #C:\Users\kouev\honey-static-webapp
+
