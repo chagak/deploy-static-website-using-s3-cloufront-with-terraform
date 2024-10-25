5b27c78b597b4e0885c59290f04eb5c02e0164d3
chagak <kchaggak@gmail.com>
2024-10-25 14:16:19 -0500

    new update

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,11 +4,11 @@ resource "aws_s3_bucket" "example" {
 }
 
 # Upload files to S3 using local-exec
-resource "null_resource" "upload_docker_folder" {
-  provisioner "local-exec" {
-    command = "aws s3 sync ./honey-static-webapp s3://${aws_s3_bucket.example.bucket} --acl private"
-  }
+# resource "null_resource" "upload_docker_folder" {
+#   provisioner "local-exec" {
+#     command = "aws s3 sync ./honey-static-webapp s3://${aws_s3_bucket.example.bucket} --acl private"
+#   }
 
-  depends_on = [aws_s3_bucket.example]
-}
-
+#   depends_on = [aws_s3_bucket.example]
+# }
+## Let try again
